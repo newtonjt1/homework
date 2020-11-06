239d6d307fd3d30926f80f6acfdd81536aa01e9d
newtonjt1 <72168444+newtonjt1@users.noreply.github.com>
2020-11-05 21:14:18 -0500

    Add files via upload

---
+++ homework/form.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!--form.php-->
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>PHP check</title>
+	</head>
+	<body>
+		<p>Subject 1: <?php print( $_POST['subj1'] ); ?><br>
+			Work completed: <?php print( $_POST['subj1work'] ); ?>
+		</p>
+		
+		<p>Subject 2: <?php print( $_POST['subj2'] ); ?><br>
+			Work completed: <?php print( $_POST['subj2work'] ); ?>
+		</p>
+
+		<p>Subject 3: <?php print( $_POST['subj3'] ); ?><br>
+			Work completed: <?php print( $_POST['subj3work'] ); ?>
+		</p>
+
+		<p>Subject 4: <?php print( $_POST['subj4'] ); ?><br>
+			Work completed: <?php print( $_POST['subj4work'] ); ?>
+		</p>
+
+		<p>Subject 5: <?php print( $_POST['subj5'] ); ?><br>
+			Work completed: <?php print( $_POST['subj5work'] ); ?>
+		</p>
+
+		<p>Subject 6: <?php print( $_POST['subj6'] ); ?><br>
+			Work completed: <?php print( $_POST['subj6work'] ); ?>
+		</p>
+
+		<p>Subject 7: <?php print( $_POST['subj7'] ); ?><br>
+			Work completed: <?php print( $_POST['subj7work'] ); ?>
+		</p>
+
+		<p>Subject 8: <?php print( $_POST['subj8'] ); ?><br>
+			Work completed: <?php print( $_POST['subj8work'] ); ?>
+		</p>
+
+	</body>
+</html>
